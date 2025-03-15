e3cbe70c80ff7b174185bb139c804f0f4aa2f44c
ahmsay <ahmsay@gmail.com>
2025-03-15 15:02:09 +0300

    lambda func is packaged

---
+++ server/lambda_function.py
@@ -0,0 +1,12 @@
+import json
+import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from openai import OpenAI
+import chromadb
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
