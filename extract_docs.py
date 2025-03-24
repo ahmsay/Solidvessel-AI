d716e4905c96045e9379c836bee2475cd6dd82fe
ahmsay <ahmsay@gmail.com>
2025-03-24 22:09:30 +0300

    extract docs is refactored

--- extract_docs.py
+++ extract_docs.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import chromadb
 
-load_dotenv()
-
 def read_files_from_directory(directory, file_extensions=(".md")):
     repo_data = {}
     for root, _, files in os.walk(directory):
@@ -16,28 +14,32 @@ def read_files_from_directory(directory, file_extensions=(".md")):
                     repo_data[file_path] = f.read()
     return repo_data
 
+def split_to_chunks():
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    docs_chunks = []
+    for file_path, content in docs.items():
+        chunks = splitter.split_text(content)
+        for i, chunk in enumerate(chunks):
+            docs_chunks.append({"source": file_path, "chunk_id": i, "content": chunk})
+    return docs_chunks
+
+def add_embeddings():
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    for chunk in docs_chunks:
+        embedding = client.embeddings.create(
+            input=chunk["content"], model="text-embedding-3-small"
+        ).data[0].embedding
+
+        collection.add(
+            documents=[chunk["content"]],
+            metadatas=[{"source": chunk["source"], "chunk_id": chunk["chunk_id"]}],
+            ids=[f"{chunk['source']}_{chunk['chunk_id']}"],
+            embeddings=[embedding]
+        )
+
+load_dotenv()
 docs = read_files_from_directory(os.getenv("REPO_PATH"))
-
-splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-docs_chunks = []
-for file_path, content in docs.items():
-    chunks = splitter.split_text(content)
-    for i, chunk in enumerate(chunks):
-        docs_chunks.append({"source": file_path, "chunk_id": i, "content": chunk})
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+docs_chunks = split_to_chunks()
 vector_db = chromadb.PersistentClient(path="./chroma_db")
-
 collection = vector_db.get_or_create_collection(name="solidvessel_docs")
-
-for chunk in docs_chunks:
-    embedding = client.embeddings.create(
-        input=chunk["content"], model="text-embedding-3-small"
-    ).data[0].embedding
-
-    collection.add(
-        documents=[chunk["content"]],
-        metadatas=[{"source": chunk["source"], "chunk_id": chunk["chunk_id"]}],
-        ids=[f"{chunk['source']}_{chunk['chunk_id']}"],
-        embeddings=[embedding]
-    )
+add_embeddings()
