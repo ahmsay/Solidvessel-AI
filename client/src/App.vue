ecd9afa9dd5c147588c6a122d27042d743f2de05
ahmsay <ahmsay@gmail.com>
2025-06-05 14:28:37 +0300

    basic style for now

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,5 +11,22 @@
 </template>
 
 <style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
 
+div {
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  margin: 0;
+  padding: 0;
+}
 </style>
